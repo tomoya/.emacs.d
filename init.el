070f3f36879c4d87c6779f6c2b57b46eb1647a49
tomoya <tomoya.ton@gmail.com>
2018-12-26 11:36:06 +0900

    * Emacs (Config): create init.el and set backup dir

---
+++ init.el
@@ -0,0 +1,20 @@
+(defvar user-backup-directory (locate-user-emacs-file "backups")
+  "This variable is define backup directory for users")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms
+   `(("\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'" ,user-backup-directory t)))
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
