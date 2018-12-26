f60a8958a62614765954abedd0300f9eb212359f
tomoya <tomoya.ton@gmail.com>
2018-12-26 11:03:03 +0900

    * Emacs (Config): add early-init.el and didable tool-bar and more

---
+++ early-init.el
@@ -0,0 +1,12 @@
+(message "Start early initialization...")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+(message "Done early initialization")
