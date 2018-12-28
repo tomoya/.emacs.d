4703e571d5216a5ffc492ccfe7e11d6b1ac0b950
tomoya <tomoya.ton@gmail.com>
2018-12-28 17:52:53 +0900

    * Emacs (Package): add ddskk

---
+++ elpa/ccc-20151205.1343/ccc-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ccc" "20151205.1343" "buffer local cursor color control library" 'nil :commit "cc3abce75d196d0634270c01ced44a63e7713d8a" :keywords '("cursor") :authors '(("Masatake YAMATO" . "masata-y@is.aist-nara.ac.jp")) :maintainer '("SKK Development Team" . "skk@ring.gr.jp") :url "https://github.com/skk-dev/ddskk/blob/master/READMEs/README.ccc.org")
