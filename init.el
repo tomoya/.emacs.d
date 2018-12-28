1ec9fe3a9cf4bcbaf7628215aad994ed915d88e5
tomoya <tomoya.ton@gmail.com>
2018-12-28 16:19:51 +0900

    * Emacs (Config): add PATH to exac-path

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+(require 'cl-lib)
+
+;; Set exec-path from PATH env
+(cl-loop for x in (reverse
+                (split-string (substring (shell-command-to-string "echo $PATH") 0 -1) ":"))
+      do (add-to-list 'exec-path x))
+
 (defvar user-backup-directory (locate-user-emacs-file "backups")
   "This variable is define backup directory for users")
 
