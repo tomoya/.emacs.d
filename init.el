d8a88966cfb21034dceea2b64f614a64c61fc4e9
tomoya <tomoya.ton@gmail.com>
2018-12-30 03:23:19 +0900

    * Emacs (Config): User local lint

--- init.el
+++ init.el
@@ -108,6 +108,19 @@
 
 (add-hook 'typescript-mode-hook #'setup-tide-mode)
 
+(defun my-use-local-lint ()
+  "Use local lint if exist it"
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory) "node_modules"))
+         (eslint (and root (expand-file-name "node_modules/.bin/eslint" root)))
+         (tslint (and root (expand-file-name "node_modules/.bin/tslint" root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))
+    (when (and tslint (file-executable-p tslint))
+      (setq-local flycheck-typescript-tslint-executable tslint))))
+
+(add-hook 'flycheck-mode-hook #'my-use-local-lint)
+
 ;; Key bindings
 (define-key global-map (kbd "s-t") 'helm-for-files)
 (define-key global-map (kbd "s-p") 'helm-ghq)
