d2b9ee576d699684eebb48f1db305ca135543de2
tomoya <tomoya.ton@gmail.com>
2018-12-30 03:08:00 +0900

    * Emacs (Config): setq PATH env

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (require 'cl-lib)
 
-;; Set exec-path from PATH env
+;; Set PATH env and exec-path from $PATH
+(setenv "PATH" (shell-command-to-string "echo $PATH"))
 (cl-loop for x in (reverse
                 (split-string (substring (shell-command-to-string "echo $PATH") 0 -1) ":"))
       do (add-to-list 'exec-path x))
