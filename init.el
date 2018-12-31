fa45776ecd13c0fa2845b2f32330b0fbf521c17d
tomoya <tomoya.ton@gmail.com>
2019-01-01 03:36:45 +0900

    * Emacs (Config): change auto-revert-interval to 1 from 4

--- init.el
+++ init.el
@@ -16,6 +16,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-revert-interval 1)
  '(auto-save-file-name-transforms
    `(("\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'" ,user-backup-directory t)))
  '(company-auto-complete t)
