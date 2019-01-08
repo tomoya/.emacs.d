5c27e5202fc856cfbd4e421b2d98ba0e608439f4
tomoya <tomoya.ton@gmail.com>
2019-01-08 11:54:47 +0900

    * Emacs (Config): add frame config for before initialization

--- early-init.el
+++ early-init.el
@@ -5,6 +5,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe-mode '(nil . 0) nil (fringe))
+ '(inhibit-startup-screen t)
+ '(initial-frame-alist
+   '((width . 238)
+     (height . 72)
+     (top . 0)
+     (left . 0)
+     (left-fringe . 0)))
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
