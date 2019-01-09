3e4d592995a2e7a4a75b8e4c0741af76f1d7c1c7
tomoya <tomoya.ton@gmail.com>
2019-01-09 16:47:07 +0900

    * Emacs (Config): disaale newline mark for lsp-ui

--- init.el
+++ init.el
@@ -100,7 +100,7 @@
                [92 9])))
  '(whitespace-space-regexp "\\(　+\\)")
  '(whitespace-style
-   '(face trailing tabs spaces lines newline empty indentation big-indent space-after-tab space-before-tab space-mark tab-mark newline-mark)))
+   '(face trailing tabs spaces lines newline empty indentation big-indent space-after-tab space-before-tab space-mark tab-mark)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
