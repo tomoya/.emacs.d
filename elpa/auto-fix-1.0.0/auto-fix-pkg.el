690bca212296c49aaaa9ccee4e604eb057379057
tomoya <tomoya.ton@gmail.com>
2019-01-20 12:58:21 +0900

    * Emacs (Package): add auto-fix

---
+++ elpa/auto-fix-1.0.0/auto-fix-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "auto-fix" "1.0.0" "Fix current buffer automatically" '((emacs "26.1")) :authors '(("Tomoya Otake" . "tomoya.ton@gmail.com")) :maintainer '("Tomoya Otake" . "tomoya.ton@gmail.com") :url "https://github.com/tomoya/auto-fix/")
