e7f6ba3ca6b69672cc65d4cfd42ecc75c8bd1060
tomoya <tomoya.ton@gmail.com>
2019-01-24 16:34:57 +0900

    * Emacs (Package): update
    - company-lsp
    - lsp-mode
    - dash
    - flycheck
    - helm
    - helm-core
    - magit

---
+++ elpa/lsp-mode-20190124.644/lsp-mode-autoloads.el
@@ -0,0 +1,57 @@
+;;; lsp-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "lsp-clients"
+;;;;;;  "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-clients.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-clients.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lsp-clients" '("fortls--suggest-project-root" "lsp-")))
+
+;;;***
+
+;;;### (autoloads nil "lsp-mode" "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode.el"
+;;;;;;  "49cce3e35dd62c2f1ae2d5d7b46c36f2")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode.el
+
+(autoload 'lsp "lsp-mode" "\
+Entry point for the server startup.
+When IGNORE-MULTI-FOLDER is t the lsp mode will start new
+language server even if there is language server which can handle
+current language. When IGNORE-MULTI-FOLDER is nil current file
+will be openned in multi folder language server if there is
+such.
+
+\(fn &optional IGNORE-MULTI-FOLDER)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "lsp-mode" "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "lsp-mode" '("lsp-" "make-lsp-client" "when-lsp-workspace" "with-lsp-workspace")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-clients.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode-pkg.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp-mode.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/lsp-mode-20190124.644/lsp.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; lsp-mode-autoloads.el ends here
