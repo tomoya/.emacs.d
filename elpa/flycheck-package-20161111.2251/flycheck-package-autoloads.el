6334af4c7af1ca7e364c2bbfa4cc8988ecaf7f42
tomoya <tomoya.ton@gmail.com>
2019-01-30 12:18:28 +0900

    * Emacs (Package): add flycheck-package

---
+++ elpa/flycheck-package-20161111.2251/flycheck-package-autoloads.el
@@ -0,0 +1,42 @@
+;;; flycheck-package-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-package" "../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package.el"
+;;;;;;  "46951d3509372d6f792220e34667914e")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package.el
+
+(autoload 'flycheck-package-setup "flycheck-package" "\
+Setup flycheck-package.
+Add `flycheck-emacs-lisp-package' to `flycheck-checkers'.
+
+\(fn)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "flycheck-package"
+;;;;;;  "../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-package" '("flycheck-package--start")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/flycheck-package-20161111.2251/flycheck-package.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-package-autoloads.el ends here
--- init.el
+++ init.el
@@ -72,7 +72,7 @@
    '(("melpa" . "https://melpa.org/packages/")
      ("gnu" . "https://elpa.gnu.org/packages/")))
  '(package-selected-packages
-   '(easy-hugo htmlize markdown-preview-mode auto-fix wgrep company-emoji company-lsp lsp-ui lsp-mode web-mode direnv darkburn-theme undo-tree markdown-mode smartparens tide typescript-mode ddskk company-flx helm-ghq company helm-projectile projectile magit helm-descbinds helm diff-hl spacemacs-theme))
+   '(flycheck-package easy-hugo htmlize markdown-preview-mode auto-fix wgrep company-emoji company-lsp lsp-ui lsp-mode web-mode direnv darkburn-theme undo-tree markdown-mode smartparens tide typescript-mode ddskk company-flx helm-ghq company helm-projectile projectile magit helm-descbinds helm diff-hl spacemacs-theme))
  '(projectile-mode t nil (projectile))
  '(recentf-max-saved-items 100000)
  '(recentf-mode t)
