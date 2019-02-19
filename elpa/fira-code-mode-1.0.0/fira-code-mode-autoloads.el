3605768a095c09b2cc58bb48011ade234fa1a3ec
tomoya <tomoya.ton@gmail.com>
2019-02-19 15:02:30 +0900

    * Emacs (Package): add fira-code-mode.el

---
+++ elpa/fira-code-mode-1.0.0/fira-code-mode-autoloads.el
@@ -0,0 +1,51 @@
+;;; fira-code-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "fira-code-mode" "../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode.el"
+;;;;;;  "8262da48cefa8a105942cbd0d4fbe21a")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode.el
+
+(autoload 'fira-code-mode "fira-code-mode" "\
+Fira Code ligatures minor mode
+
+If called interactively, enable Fira-Code mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+(autoload 'fira-code-mode--setup "fira-code-mode" "\
+Setup Fira Code Symbols
+
+\(fn)" nil nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "fira-code-mode"
+;;;;;;  "../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fira-code-mode" '("fira-code-mode--")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/fira-code-mode-1.0.0/fira-code-mode.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; fira-code-mode-autoloads.el ends here
