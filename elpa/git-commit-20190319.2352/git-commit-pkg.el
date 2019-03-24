46f895aef59214a6b95d01759cf3442f4fc34227
tomoya <tomoya.ton@gmail.com>
2019-03-24 12:09:33 +0900

    Update packages
    - all-the-icons-20190320.1809
    - dash-20190320.1406
    - direnv-20190319.1418
    - flycheck-20190320.1026
    - git-commit-20190319.2352
    - helm-core-20190323.748
    - lsp-mode-20190323.612
    - magit-20190323.2151
    - projectile-20190323.904
    - rspec-mode-20190319.1415
    - tide-20190321.1611
    - transient-20190319.1928

---
+++ elpa/git-commit-20190319.2352/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/git-commit-20190319.2352/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190319.2352" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "217f1a7da05778418a45472115845b4ab3adaf5d" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
