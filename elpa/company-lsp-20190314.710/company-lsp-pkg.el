88d19fe4e50240cf661ad908bfffd9eda4d7826c
tomoya <tomoya.ton@gmail.com>
2019-03-28 14:30:26 +0900

    Update packages
    - company-lsp-20190326.553
    - flycheck-posframe-20190327.1111
    - helm-20190326.1022
    - helm-core-20190326.921
    - lsp-mode-20190327.1921
    - magit-20190326.2143
    - projectile-20190326.759
    - rspec-mode-20190326.1422

--- elpa/company-lsp-20190314.710/company-lsp-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.emacs.d/elpa/company-lsp-20190314.710/company-lsp.el  -*- no-byte-compile: t -*-
-(define-package "company-lsp" "20190314.710" "Company completion backend for lsp-mode." '((emacs "25.1") (lsp-mode "6.0") (company "0.9.0") (s "1.2.0") (dash "2.11.0")) :commit "dcea879c488057faa1063f0804c622d6b8794b61" :url "https://github.com/tigersoldier/company-lsp")
---
+++ elpa/flycheck-posframe-20190327.1111/flycheck-posframe-autoloads.el
@@ -0,0 +1,49 @@
+;;; flycheck-posframe-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "flycheck-posframe" "../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe.el"
+;;;;;;  "0a2893a4b8d2fbfd92e51e51c50b6573")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe.el
+
+(autoload 'flycheck-posframe-configure-pretty-defaults "flycheck-posframe" "\
+Configure some nicer settings for prettier display." nil nil)
+
+(autoload 'flycheck-posframe-mode "flycheck-posframe" "\
+A minor mode to show Flycheck error messages in a posframe.
+
+If called interactively, enable Flycheck-Posframe mode if ARG is positive, and
+disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it
+if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "flycheck-posframe"
+;;;;;;  "../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-posframe" '("flycheck-posframe-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/flycheck-posframe-20190327.1111/flycheck-posframe.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; flycheck-posframe-autoloads.el ends here
