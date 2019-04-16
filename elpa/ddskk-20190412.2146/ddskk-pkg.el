9745de38e2aac2c8641e053b26ade11100922d6d
tomoya <tomoya.ton@gmail.com>
2019-04-16 14:51:21 +0900

    Update packages
    - company-20190415.203
    - dash-20190413.1058
    - ddskk-20190412.2146
    - flycheck-20190411.1707
    - git-commit-20190413.1201
    - helm-20190413.703
    - lsp-mode-20190415.85
    - lsp-ui-20190413.1606
    - magit-20190413.1201
    - pdf-tools-20190413.2018
    - projectile-20190415.1652
    - tablist-20190414.643
    - web-mode-20190415.1950

---
+++ elpa/ddskk-20190412.2146/ddskk-pkg.el
@@ -0,0 +1,6 @@
+(define-package "ddskk" "20190412.2146" "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/projectile-20190415.1652/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/projectile-20190415.1652/projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "20190415.1652" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "b54be3e3d7ab2eaedd34532209d34dd1cd4e5688" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
