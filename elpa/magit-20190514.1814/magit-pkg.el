451aac1b2b467e54b9b5a02ccb604a4c0520c1ed
tomoya <tomoya.ton@gmail.com>
2019-05-15 15:34:45 +0900

    Update packages
    - helm-20190514.1137
    - helm-core-20190514.659
    - lsp-mode-20190514.1523
    - magit-20190514.1814

--- elpa/magit-20190514.1814/magit-pkg.el
+++ elpa/magit-20190514.1814/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20190512.1914" "A Git porcelain inside Emacs."
+(define-package "magit" "20190514.1814" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (async "20180527")
     (dash "20180910")
