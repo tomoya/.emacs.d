88548ac7cf492105a023b92ce3aad6d19006368a
tomoya <tomoya.ton@gmail.com>
2019-05-22 23:18:06 +0900

    Update packages
    - lsp-mode-20190522.959
    - magit-20190522.833
    - php-mode-20190522.1206
    - web-mode-20190522.610

--- elpa/web-mode-20190506.607/web-mode-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "web-mode" "../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode.el"
-;;;;;;  "6ee06423d9cff7670ad7b77d7d01a37e")
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "web-mode" "../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode" '("web-mode-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode-autoloads.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/web-mode-20190506.607/web-mode.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; web-mode-autoloads.el ends here
