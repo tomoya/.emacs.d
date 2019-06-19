750ea152b2f1e5b027b2230ace585c5861f11442
tomoya <tomoya.ton@gmail.com>
2019-06-19 10:59:26 +0900

    Update packages
    - lsp-mode-20190618.848
    - magit-20190618.1350
    - package-lint-20190617.315
    - projectile-20190617.1338
    - spacemacs-theme-20190617.633
    - tide-20190617.1308

---
+++ elpa/projectile-20190617.1338/projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/projectile-20190617.1338/projectile.el  -*- no-byte-compile: t -*-
+(define-package "projectile" "20190617.1338" "Manage and navigate projects in Emacs easily" '((emacs "25.1") (pkg-info "0.4")) :commit "b46bb7070f3745ef8b35785afa27eb3e9f13353a" :keywords '("project" "convenience") :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "https://github.com/bbatsov/projectile")
