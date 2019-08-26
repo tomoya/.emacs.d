035f4652edbf5afdbb0e1bae675d9244af324b3b
tomoya <tomoya.ton@gmail.com>
2019-08-26 15:40:04 +0900

    Add company-tabnine

---
+++ elpa/company-tabnine-20190811.2013/company-tabnine-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/company-tabnine-20190811.2013/company-tabnine.el  -*- no-byte-compile: t -*-
+(define-package "company-tabnine" "20190811.2013" "A company-mode backend for TabNine" '((emacs "25") (company "0.9.3") (cl-lib "0.5") (dash "2.16.0") (s "1.12.0") (unicode-escape "1.1")) :commit "35f758bff33e9d50535d23eaddb6d6ecdc03bf7d" :keywords '("convenience") :authors '(("Tommy Xiang" . "tommyx058@gmail.com")) :maintainer '("Tommy Xiang" . "tommyx058@gmail.com") :url "https://github.com/TommyX12/company-tabnine/")
---
+++ elpa/unicode-escape-20160614.1234/unicode-escape-autoloads.el
@@ -0,0 +1,39 @@
+;;; unicode-escape-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "unicode-escape" "../../../../../../../.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape.el"
+;;;;;;  "28a0854025c33b0b674e8d36889c4fe6")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape.el
+
+(autoload 'unicode-escape "unicode-escape" nil nil nil)
+
+(autoload 'unicode-escape* "unicode-escape" nil nil nil)
+
+(autoload 'unicode-unescape "unicode-escape" nil nil nil)
+
+(autoload 'unicode-unescape* "unicode-escape" nil nil nil)
+
+(autoload 'unicode-escape-region "unicode-escape" nil nil nil)
+
+(autoload 'unicode-unescape-region "unicode-escape" nil nil nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/unicode-escape-20160614.1234/unicode-escape.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; unicode-escape-autoloads.el ends here
