26b6a15b811ee44f7ee6229315d704e824693d4d
tomoya <tomoya.ton@gmail.com>
2019-08-27 10:31:50 +0900

    Update packages
    - lsp-mode-20190826.1548
    - magit-20190825.1416
    - php-mode-20190826.1108

--- elpa/lsp-mode-20190826.1548/lsp-mode-pkg.el
+++ elpa/lsp-mode-20190826.1548/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20190822.702" "LSP mode"
+(define-package "lsp-mode" "20190826.1548" "LSP mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
--- elpa/php-mode-20190826.1108/php-mode-pkg.el
+++ elpa/php-mode-20190826.1108/php-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "php-mode" "20190824.438" "Major mode for editing PHP code"
+(define-package "php-mode" "20190826.1108" "Major mode for editing PHP code"
   '((emacs "24.3")
     (cl-lib "0.5"))
   :keywords
