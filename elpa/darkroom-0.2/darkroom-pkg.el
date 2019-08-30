d10536c0eb9b5b7e62fd52c3bf82bdc6854d13e7
tomoya <tomoya.ton@gmail.com>
2019-08-31 04:41:35 +0900

    Add darkroom-mode

---
+++ elpa/darkroom-0.2/darkroom-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/darkroom-0.2/darkroom.el  -*- no-byte-compile: t -*-
+(define-package "darkroom" "0.2" "Remove visual distractions and focus on writing" '((cl-lib "0.5")) :url "http://elpa.gnu.org/packages/darkroom.html" :keywords '("convenience" "emulations") :authors '(("João Távora" . "joaotavora@gmail.com")) :maintainer '("João Távora" . "joaotavora@gmail.com"))
