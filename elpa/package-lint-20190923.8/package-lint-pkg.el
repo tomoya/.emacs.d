92e1f2e80d57659fcece495608d7147b02e73d60
tomoya <tomoya.ton@gmail.com>
2019-09-24 04:57:23 +0900

    Update packages
    - dash-20190920.1035
    - helm-20190923.1807
    - lsp-mode-20190922.1013
    - package-lint-20190923.8
    - pdf-tools-20190918.1715
    - php-mode-20190919.1405
    - request-20190923.1502

---
+++ elpa/package-lint-20190923.8/package-lint-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.emacs.d/elpa/package-lint-20190923.8/package-lint.el  -*- no-byte-compile: t -*-
+(define-package "package-lint" "20190923.8" "A linting library for elisp package authors" '((cl-lib "0.5") (emacs "24")) :commit "f00e453b7d028d70afe459a09e895e3a67cc7291" :keywords '("lisp") :authors '(("Steve Purcell" . "steve@sanityinc.com") ("Fanael Linithien" . "fanael4@gmail.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/package-lint")
