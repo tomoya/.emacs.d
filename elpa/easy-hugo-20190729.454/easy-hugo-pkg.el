e8569e06889f0cb9e98e90ae849ad3cd1949e03e
tomoya <tomoya.ton@gmail.com>
2019-10-15 05:25:45 +0900

    Update packages
    - easy-hugo-20191013.1814
    - helm-20191010.1701
    - helm-core-20191012.1704
    - lsp-mode-20191013.1943
    - magit-20191014.1138
    - memoize-20191004.351
    - posframe-20191013.756
    - request-20191014.1417
    - transient-20191011.1811

--- elpa/easy-hugo-20190729.454/easy-hugo-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.emacs.d/elpa/easy-hugo-20190729.454/easy-hugo.el  -*- no-byte-compile: t -*-
-(define-package "easy-hugo" "20190729.454" "Write blogs made with hugo by markdown or org-mode" '((emacs "24.4") (popup "0.5.3") (request "0.3.0")) :commit "2e2eb5720792512bb8a2ab2a7d9eb9ce86de8df9" :authors '(("Masashı Mıyaura")) :maintainer '("Masashı Mıyaura") :url "https://github.com/masasam/emacs-easy-hugo")
---
+++ elpa/memoize-20191004.351/memoize-autoloads.el
@@ -0,0 +1,29 @@
+;;; memoize-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "memoize" "../../../../../../../.emacs.d/elpa/memoize-20191004.351/memoize.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/memoize-20191004.351/memoize.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "memoize" '("defmemoize" "memoize")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/memoize-20191004.351/memoize-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/memoize-20191004.351/memoize.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; memoize-autoloads.el ends here
