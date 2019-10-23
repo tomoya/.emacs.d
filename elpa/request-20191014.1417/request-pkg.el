e4dae4252bab633fd12f410b39dc87faf73d37d3
tomoya <tomoya.ton@gmail.com>
2019-10-24 01:16:17 +0900

    Update packages
    - flycheck-20191022.1117
    - helm-20191023.651
    - lsp-mode-20191023.518
    - magit-20191022.1848
    - projectile-20191021.1201
    - request-20191022.615

--- elpa/request-20191014.1417/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.emacs.d/elpa/request-20191014.1417/request.el  -*- no-byte-compile: t -*-
-(define-package "request" "20191014.1417" "Compatible layer for URL request in Emacs" '((emacs "24.4")) :commit "1632fbc1f98ab2fbfaf6ee8407f2a907ec2065e9" :authors '(("Takafumi Arakaki <aka.tkf at gmail.com>")) :maintainer '("Takafumi Arakaki <aka.tkf at gmail.com>") :url "https://github.com/tkf/emacs-request")
