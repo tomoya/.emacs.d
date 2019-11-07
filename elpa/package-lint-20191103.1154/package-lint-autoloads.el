70df49843f3c35b153beb74f7e9e242926edb77c
tomoya <tomoya.ton@gmail.com>
2019-11-07 23:01:46 +0900

    Update packages
    - company-20191106.2247
    - dash-20191102.2142
    - git-commit-20191107.857
    - helm-20191107.820
    - helm-core-20191107.636
    - lsp-mode-20191107.333
    - magit-20191105.2244
    - package-lint-20191103.1154
    - posframe-20191102.1450
    - transient-20191105.2249
    - web-mode-20191104.1859
    - with-editor-20191105.2250

---
+++ elpa/package-lint-20191103.1154/package-lint-autoloads.el
@@ -0,0 +1,63 @@
+;;; package-lint-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "package-lint" "../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint.el"
+;;;;;;  "8ae78f046419721e7af271b1f0a167bb")
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint.el
+
+(autoload 'package-lint-buffer "package-lint" "\
+Get linter errors and warnings for BUFFER.
+
+Returns a list, each element of which is list of
+
+   (LINE COL TYPE MESSAGE)
+
+where TYPE is either 'warning or 'error.
+
+Current buffer is used if none is specified.
+
+\(fn &optional BUFFER)" nil nil)
+
+(autoload 'package-lint-current-buffer "package-lint" "\
+Display lint errors and warnings for the current buffer." t nil)
+
+(autoload 'package-lint-batch-and-exit "package-lint" "\
+Run `package-lint-buffer' on the files remaining on the command line.
+Use this only with -batch, it won't work interactively.
+
+When done, exit Emacs with status 0 if there were no errors nor warnings or 1
+otherwise." nil nil)
+
+(autoload 'package-lint-looks-like-a-package-p "package-lint" "\
+Return non-nil if the current buffer appears to be intended as a package." nil nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "package-lint"
+;;;;;;  "../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "package-lint" '("package-lint-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint-autoloads.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint-pkg.el"
+;;;;;;  "../../../../../../../.emacs.d/elpa/package-lint-20191103.1154/package-lint.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; package-lint-autoloads.el ends here
