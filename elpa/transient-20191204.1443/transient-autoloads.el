cd2f62c6adf04c6ed24987227fe22bd003915119
tomoya <tomoya.ton@gmail.com>
2019-12-05 20:46:45 +0900

    Update packages
    - company-20191205.53
    - helm-20191203.642
    - helm-core-20191203.642
    - lsp-mode-20191205.725
    - php-mode-20191205.243
    - projectile-20191204.1451
    - transient-20191204.1443

---
+++ elpa/transient-20191204.1443/transient-autoloads.el
@@ -0,0 +1,30 @@
+;;; transient-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "transient" "../../../../../../../.config/emacs/elpa/transient-20191204.1443/transient.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/transient-20191204.1443/transient.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "transient" '("current-transient-" "define-" "post-transient-hook" "transient-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/transient-20191204.1443/transient-autoloads.el"
+;;;;;;  "../../../../../../../.config/emacs/elpa/transient-20191204.1443/transient-pkg.el"
+;;;;;;  "../../../../../../../.config/emacs/elpa/transient-20191204.1443/transient.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; transient-autoloads.el ends here
