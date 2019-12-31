6883d1da02948fb6d74b62759c328be5869f1404
tomoya <tomoya.ton@gmail.com>
2020-01-01 03:24:47 +0900

    Update packages
    - async-20191226.1227
    - company-emoji-20191226.1915
    - git-commit-20191227.1317
    - helm-20191231.824
    - lsp-mode-20191231.158
    - lsp-ui-20191228.1151
    - magit-20191229.1345
    - markdown-mode-20191230.1055
    - package-lint-20191220.419
    - pdf-tools-20191228.1005
    - php-mode-20191230.1606
    - spacemacs-theme-20191227.1039
    - transient-20191226.1938

--- elpa/spacemacs-theme-20191107.1747/spacemacs-theme-autoloads.el
+++
@@ -1,59 +0,0 @@
-;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "spacemacs-common" "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-common.el"
-;;;;;;  "34d7fa9500d0740075047e280a481397")
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;### (autoloads "actual autoloads are elsewhere" "spacemacs-common"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-common.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-common.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spacemacs-common" '("create-spacemacs-theme" "spacemacs-theme-" "true-color-p")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads "actual autoloads are elsewhere" "spacemacs-dark-theme"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-dark-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-dark-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spacemacs-dark-theme" '("spacemacs-dark")))
-
-;;;***
-
-;;;### (autoloads "actual autoloads are elsewhere" "spacemacs-light-theme"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-light-theme.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-light-theme.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "spacemacs-light-theme" '("spacemacs-light")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-common.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-dark-theme.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-light-theme.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-theme-autoloads.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/spacemacs-theme-20191107.1747/spacemacs-theme-pkg.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; spacemacs-theme-autoloads.el ends here
