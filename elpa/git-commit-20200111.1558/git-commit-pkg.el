74945ea01c9c4a9b5295f9890212addff767c366
tomoya <tomoya.ton@gmail.com>
2020-01-12 21:30:51 +0900

    Update packages
    - company-20200110.929
    - git-commit-20200111.1558
    - lsp-mode-20200111.839
    - magit-20200111.1558
    - tide-20200111.1236
    - undo-tree-0.7.2

---
+++ elpa/git-commit-20200111.1558/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.config/emacs/elpa/git-commit-20200111.1558/git-commit.el  -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200111.1558" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (transient "20190812") (with-editor "20181103")) :commit "4d6bc190b2334fa226035d13fcdfce3cca55d7be" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
