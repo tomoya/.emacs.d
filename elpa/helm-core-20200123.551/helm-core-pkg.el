43ef38ced0cb38900280692b59c279a1f4ea2c22
tomoya <tomoya.ton@gmail.com>
2020-01-27 17:35:10 +0900

    Update packages
    - git-commit-20200123.1706
    - helm-20200126.1804
    - helm-core-20200126.1756
    - lsp-mode-20200126.1714
    - magit-20200126.1750
    - projectile-20200125.1343
    - request-20200127.21
    - transient-20200125.1308
    - undo-tree-0.7.3

--- elpa/helm-core-20200123.551/helm-core-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "helm-core" "20200123.551" "Development files for Helm"
-  '((emacs "24.4")
-    (async "1.9.4"))
-  :url "https://emacs-helm.github.io/helm/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/transient-20200107.2156/transient-autoloads.el
+++
@@ -1,30 +0,0 @@
-;;; transient-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads "actual autoloads are elsewhere" "transient" "../../../../../../../.config/emacs/elpa/transient-20200107.2156/transient.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/transient-20200107.2156/transient.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "transient" '("current-transient-" "define-" "post-transient-hook" "transient-")))
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/transient-20200107.2156/transient-autoloads.el"
-;;;;;;  "../../../../../../../.config/emacs/elpa/transient-20200107.2156/transient-pkg.el"
-;;;;;;  "../../../../../../../.config/emacs/elpa/transient-20200107.2156/transient.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; transient-autoloads.el ends here
