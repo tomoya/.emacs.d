f93976910897dc780e54088f79727364fc965abc
tomoya <tomoya.ton@gmail.com>
2020-02-12 13:47:57 +0900

    Update packages
    - cdb-20200210.1326
    - ddskk-20200210.2117
    - lsp-mode-20200211.1904
    - lsp-ui-20200211.804

--- elpa/cdb-20151205.1343/cdb-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cdb" "20151205.1343" "constant database (cdb) reader for Emacs Lisp" 'nil :commit "cc3abce75d196d0634270c01ced44a63e7713d8a" :keywords '("cdb") :authors '(("Yusuke Shinyama <yusuke at cs . nyu . edu>")) :maintainer '("SKK Development Team" . "skk@ring.gr.jp"))
