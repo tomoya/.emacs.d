5b902396d4983431ecb3df780c8307a2b7f8934b
tomoya <tomoya.ton@gmail.com>
2020-02-17 00:32:47 +0900

    Add M-g p/n keybindings to diff-hl-previsous/next-hunk

--- init.el
+++ init.el
@@ -445,6 +445,8 @@ If buffer is associated with a file name, add that file to the
 (global-set-key (kbd "M-g a") 'magit-stage-file)
 (global-set-key (kbd "M-g c") 'magit-commit-create)
 (global-set-key (kbd "M-g d") 'magit-diff-buffer-file)
+(global-set-key (kbd "M-g p") 'diff-hl-previous-hunk)
+(global-set-key (kbd "M-g n") 'diff-hl-next-hunk)
 (global-set-key (kbd "C-c C-r") 'quickrun)
 (global-set-key (kbd "M-s-z") 'checkout-head-revision)
 (global-set-key (kbd "C-x C-j") 'skk-mode)
