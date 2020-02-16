598ea7f29f020fe657893260d569423423950434
tomoya <tomoya.ton@gmail.com>
2020-02-17 00:28:14 +0900

    Add json-mode

---
+++ elpa/json-mode-0.1/json-mode-autoloads.el
@@ -0,0 +1,42 @@
+;;; json-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "json-mode" "../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode.el"
+;;;;;;  "527524d2fd45386a78bf0a098a898254")
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode.el
+
+(add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+
+(autoload 'json-mode "json-mode" "\
+Major mode for editing JavaScript Object Notation (JSON) data files.
+
+\(fn)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "json-mode" "../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "json-mode" '("json-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode-autoloads.el"
+;;;;;;  "../../../../../../../.config/emacs/elpa/json-mode-0.1/json-mode.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-mode-autoloads.el ends here
