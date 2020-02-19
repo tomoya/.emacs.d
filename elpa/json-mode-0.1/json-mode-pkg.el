cdaf1c54c810dc6f4e1367aecada182b0c5997a4
tomoya <tomoya.ton@gmail.com>
2020-02-20 01:03:03 +0900

    Update packages
    - csv-mode-1.12
    - ddskk-20200216.1158
    - helm-20200217.550
    - ht-20200217.2331
    - json-mode-20190123.422
    - json-reformat-20160212.853
    - json-snatcher-20150512.347
    - lsp-mode-20200218.1615
    - lsp-ui-20200217.1619
    - magit-20200217.135
    - typescript-mode-20200217.1108
    - web-server-20200216.1126
    - wgrep-20200217.1030
    - with-editor-20200217.1015

--- elpa/json-mode-0.1/json-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.config/emacs/elpa/json-mode-0.1/json-mode.el  -*- no-byte-compile: t -*-
-(define-package "json-mode" "0.1" "Major mode for editing JSON files" '((emacs "25.1")) :url "http://elpa.gnu.org/packages/json-mode.html" :keywords '("data") :authors '(("Simen Heggestøyl" . "simenheg@gmail.com")) :maintainer '("Simen Heggestøyl" . "simenheg@gmail.com"))
---
+++ elpa/json-snatcher-20150512.347/json-snatcher-autoloads.el
@@ -0,0 +1,39 @@
+;;; json-snatcher-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "json-snatcher" "../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher.el"
+;;;;;;  "308ed7c1d4c90bce864766934c6d48a9")
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher.el
+
+(autoload 'jsons-print-path "json-snatcher" "\
+Print the path to the JSON value under point, and save it in the kill ring." t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "json-snatcher"
+;;;;;;  "../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "json-snatcher" '("jsons-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher-autoloads.el"
+;;;;;;  "../../../../../../../.config/emacs/elpa/json-snatcher-20150512.347/json-snatcher.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; json-snatcher-autoloads.el ends here
