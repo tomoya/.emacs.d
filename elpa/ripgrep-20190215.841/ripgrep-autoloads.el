25ca5d87917e58fcaccc522ac1b8b2c1cb9c5528
tomoya <tomoya.ton@gmail.com>
2020-02-26 17:05:09 +0900

    Add projectile-ripgrep

---
+++ elpa/ripgrep-20190215.841/ripgrep-autoloads.el
@@ -0,0 +1,41 @@
+;;; ripgrep-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "ripgrep" "../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep.el"
+;;;;;;  "cdae88de438281386ce77a47713bc73e")
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep.el
+
+(autoload 'ripgrep-regexp "ripgrep" "\
+Run a ripgrep search with `REGEXP' rooted at `DIRECTORY'.
+`ARGS' provides Ripgrep command line arguments.
+
+\(fn REGEXP DIRECTORY &optional ARGS)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "ripgrep" "../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ripgrep" '("ripgrep")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep-autoloads.el"
+;;;;;;  "../../../../../../../.config/emacs/elpa/ripgrep-20190215.841/ripgrep.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ripgrep-autoloads.el ends here
