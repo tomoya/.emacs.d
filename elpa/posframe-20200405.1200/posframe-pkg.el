6c37ed4a8c70343209cf074152e21b34308dab92
tomoya <tomoya.ton@gmail.com>
2020-04-12 13:09:31 +0900

    Update packages
    - all-the-icons-20200411.520
    - diff-hl-20200406.37
    - helm-core-20200409.651
    - lsp-mode-20200411.1642
    - magit-20200408.2341
    - package-lint-20200408.203
    - posframe-20200408.135
    - web-mode-20200406.759

--- elpa/posframe-20200405.1200/posframe-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.config/emacs/elpa/posframe-20200405.1200/posframe.el  -*- no-byte-compile: t -*-
-(define-package "posframe" "20200405.1200" "Pop a posframe (just a frame) at point" '((emacs "26")) :commit "83b4d39fdda34355c01aecc22677f7376ae0f013" :keywords '("convenience" "tooltip") :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :url "https://github.com/tumashu/posframe")
---
+++ elpa/web-mode-20200406.759/web-mode-autoloads.el
@@ -0,0 +1,40 @@
+;;; web-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "web-mode" "../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode.el"
+;;;;;;  "75721526533ff51bcef046452f3cb383")
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode.el
+
+(autoload 'web-mode "web-mode" "\
+Major mode for editing web templates.
+
+\(fn)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "web-mode" "../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "web-mode" '("web-mode-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/web-mode-20200406.759/web-mode.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; web-mode-autoloads.el ends here
