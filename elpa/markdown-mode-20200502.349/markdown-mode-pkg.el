da064934d01cd01a0a6df9e5f18930d84abc2b4f
tomoya <tomoya.ton@gmail.com>
2020-05-06 10:07:30 +0900

    Update packages
    - flycheck-20200503.1706
    - helm-20200505.1156
    - helm-core-20200504.544
    - lsp-mode-20200504.1837
    - lsp-ui-20200503.1650
    - magit-20200503.1905
    - markdown-mode-20200504.1633
    - php-mode-20200504.711
    - tide-20200504.832
    - transient-20200503.1401

--- elpa/markdown-mode-20200502.349/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.config/emacs/elpa/markdown-mode-20200502.349/markdown-mode.el  -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20200502.349" "Major mode for Markdown-formatted text" '((emacs "25.1")) :commit "95ff45cd0bc25560ce04de3d7b98489d583743f0" :keywords '("markdown" "github flavored markdown" "itex") :authors '(("Jason R. Blevins" . "jblevins@xbeta.org")) :maintainer '("Jason R. Blevins" . "jblevins@xbeta.org") :url "https://jblevins.org/projects/markdown-mode/")
