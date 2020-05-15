4460e825350adac00f192ba6da082de7cdfcbd95
Tomoya Otake <tomoya.ton@gmail.com>
2020-05-15 09:03:39 +0900

    Update packages
    - flycheck-20200513.1939
    - helm-20200514.947
    - helm-core-20200514.644
    - lsp-mode-20200514.1955
    - magit-20200514.1330
    - markdown-mode-20200514.16
    - quickrun-20200514.1506

---
+++ elpa/quickrun-20200514.1506/quickrun-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.config/emacs/elpa/quickrun-20200514.1506/quickrun.el  -*- no-byte-compile: t -*-
+(define-package "quickrun" "20200514.1506" "Run commands quickly" '((emacs "24.3")) :commit "a8752887b81fc2f4f4e4bcd3d2cf8abafb85edbb" :authors '(("Syohei YOSHIDA" . "syohex@gmail.com")) :maintainer '("Syohei YOSHIDA" . "syohex@gmail.com") :url "https://github.com/syohex/emacs-quickrun")
