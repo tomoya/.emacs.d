cd3a2a98416ffb3fc9be8946985eebfbbe11f993
Tomoya Otake <tomoya.ton@gmail.com>
2020-06-08 01:05:35 +0900

    Update packages
    - flycheck-20200605.1351
    - helm-20200607.1000
    - lsp-mode-20200607.1346
    - magit-20200605.1926
    - posframe-20200605.1716
    - web-mode-20200605.1107

---
+++ elpa/helm-20200607.1000/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200607.1000" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "bde1791d6a4c5dc5bf650e20a7264ac990fab9ba" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20200607.1346/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20200607.1346" "LSP mode"
+  '((emacs "25.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "733f9f70ed1e13cdd9019dac755de8b5feb883fc" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
