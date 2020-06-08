cb25348f991875462aabe5a4041edc1fad389381
Tomoya Otake <tomoya.ton@gmail.com>
2020-06-08 13:08:20 +0900

    Update packages
    - helm-20200607.2009
    - lsp-mode-20200608.122
    - lsp-ui-20200607.2009

--- elpa/lsp-mode-20200608.122/lsp-mode-pkg.el
+++ elpa/lsp-mode-20200608.122/lsp-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "lsp-mode" "20200607.1346" "LSP mode"
+(define-package "lsp-mode" "20200608.122" "LSP mode"
   '((emacs "25.1")
     (dash "2.14.1")
     (dash-functional "2.14.1")
@@ -7,7 +7,7 @@
     (spinner "1.7.3")
     (markdown-mode "2.3")
     (lv "0"))
-  :commit "733f9f70ed1e13cdd9019dac755de8b5feb883fc" :keywords
+  :commit "e3ddfd356ffdc261f0eefac673f84fc0768e7caa" :keywords
   '("languages")
   :authors
   '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
