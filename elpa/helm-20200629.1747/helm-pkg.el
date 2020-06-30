7ddcaa2c37d3be5ad10e8d5719440bcc9c1372df
Tomoya Otake <tomoya.ton@gmail.com>
2020-06-30 09:38:38 +0900

    Update packages
    - helm-20200629.1747
    - helm-core-20200629.1723
    - lsp-mode-20200629.1501
    - lsp-ui-20200629.1812

---
+++ elpa/helm-20200629.1747/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200629.1747" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "4908e8f07cf0e3022b8d1aa40c9c49be02b5b61f" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
