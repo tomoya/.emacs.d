d535c56637c50c9265355285a27dd93da8d1e5e2
Tomoya Otake <tomoya.ton@gmail.com>
2020-07-13 09:42:21 +0900

    Update packages
    - helm-20200712.2011
    - helm-core-20200711.1439
    - lsp-mode-20200712.1933
    - magit-20200710.433
    - markdown-mode-20200710.1228
    - projectile-20200710.947
    - tide-20200712.807

---
+++ elpa/lsp-mode-20200712.1933/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20200712.1933" "LSP mode"
+  '((emacs "25.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "5ef8c1f586663ecf8a93a14248ddaed900b5b59b" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
