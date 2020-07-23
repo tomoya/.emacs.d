2903eeb946cd6fea52c38314f0a45b743731c66c
Tomoya Otake <tomoya.ton@gmail.com>
2020-07-23 11:54:59 +0900

    Update packages
    - git-link-20200721.2250
    - helm-20200722.1531
    - helm-core-20200722.1046
    - lsp-mode-20200722.1307
    - magit-20200715.415
    - with-editor-20200720.2014

---
+++ elpa/magit-20200715.415/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20200715.415" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "d281b17d903258d4e3b234cf94b7dce96da4c8f2" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
