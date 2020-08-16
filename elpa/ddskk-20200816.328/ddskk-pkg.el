05273eff36fee632467ec835409b28324a8408f0
Tomoya Otake <tomoya.ton@gmail.com>
2020-08-16 16:49:29 +0900

    Update packages
    - company-20200813.2229
    - ddskk-20200816.328
    - helm-20200815.1350
    - lsp-mode-20200815.2231
    - magit-20200813.957
    - markdown-mode-20200815.203
    - package-lint-20200816.24
    - php-mode-20200812.1129
    - posframe-20200814.526

---
+++ elpa/ddskk-20200816.328/ddskk-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ddskk" "20200816.328" "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754"))
+  :commit "245761b2bee75d1e065395daf75e52976848e726")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
