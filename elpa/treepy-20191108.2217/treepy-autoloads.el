8158ca5bbacdcd380d63d8b0efe4192791b9bba0
Tomoya Otake <tomoya.ton@gmail.com>
2020-08-27 23:26:15 +0900

    Add github-review package

---
+++ elpa/treepy-20191108.2217/treepy-autoloads.el
@@ -0,0 +1,29 @@
+;;; treepy-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "treepy" "../../../../../../../../../.config/emacs/elpa/treepy-20191108.2217/treepy.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/treepy-20191108.2217/treepy.el
+
+(register-definition-prefixes "treepy" '("treepy-"))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/treepy-20191108.2217/treepy-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/treepy-20191108.2217/treepy.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; treepy-autoloads.el ends here
