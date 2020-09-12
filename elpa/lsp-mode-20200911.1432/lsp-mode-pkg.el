1a9b6db26d965f98aea11d93244e65c3a289898c
Tomoya Otake <tomoya.ton@gmail.com>
2020-09-12 14:23:27 +0900

    Update packages
    - company-box-20200910.732
    - helm-20200910.1036
    - lsp-mode-20200911.1432
    - lsp-ui-20200910.1942
    - magit-20200908.1311
    - markdown-mode-20200909.2355
    - projectile-20200909.650
    - smartparens-20200911.1124

---
+++ elpa/lsp-mode-20200911.1432/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20200911.1432" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "02c64e6c6e90d36e92c848cd19b97915360aa419" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-ui-20200910.1942/lsp-ui-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lsp-ui" "20200910.1942" "UI modules for lsp-mode"
+  '((emacs "26.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "79ba8b2a901d079f5f65f87735329af400dfe692" :keywords
+  '("languages" "tools")
+  :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
