e9b255e6ef6ee66ebeadd83538e7a900cee0ce6a
Tomoya Otake <tomoya.ton@gmail.com>
2020-09-28 08:44:32 +0900

    Update packages
    - company-20200927.2222
    - company-box-20200927.1457
    - flycheck-20200926.1529
    - forge-20200927.1702
    - lsp-mode-20200927.1825
    - magit-20200927.1644
    - rg-20200926.1048
    - string-inflection-20200927.747

---
+++ elpa/magit-20200927.1644/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20200927.1644" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (async "20200113")
+    (dash "20200524")
+    (git-commit "20200516")
+    (transient "20200601")
+    (with-editor "20200522"))
+  :commit "6c1c92e5a2abbdf1c852f7da50e69f0b898e7a72" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/rg-20200926.1048/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20200926.1048" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.1.0")
+    (wgrep "2.1.10"))
+  :commit "382fdfc8f35b7a054fce478bbc3ca788786c7f2c" :keywords
+  '("matching" "tools")
+  :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
