230d3a67d1508e4f5abd7a0cd1ecceccd5059845
Tomoya Otake <tomoya.ton@gmail.com>
2020-10-06 00:29:01 +0900

    Update packages
    - company-box-20201004.735
    - forge-20201002.1420
    - lsp-mode-20201003.1959
    - lsp-ui-20201002.2002
    - magit-20201002.1340
    - rg-20201003.1556
    - typescript-mode-20201002.1109

---
+++ elpa/company-box-20201004.735/company-box-autoloads.el
@@ -0,0 +1,67 @@
+;;; company-box-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "company-box" "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box.el"
+;;;;;;  "e61a2ab06bef51b2536255e5cb8d06f0")
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box.el
+
+(autoload 'company-box-mode "company-box" "\
+Company-box minor mode.
+
+If called interactively, enable Company-Box mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
+\(fn &optional ARG)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "company-box"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-box" '("company-box-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "company-box-doc"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-doc.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-doc.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-box-doc" '("company-box-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "company-box-icons"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-icons.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-icons.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "company-box-icons" '("company-box-icons-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-doc.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-icons.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box-pkg.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/company-box-20201004.735/company-box.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; company-box-autoloads.el ends here
---
+++ elpa/rg-20201003.1556/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20201003.1556" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.1.0")
+    (wgrep "2.1.10"))
+  :commit "bbd9c183df3cb3da4b4891684ed7953e2bc9ede3" :keywords
+  '("matching" "tools")
+  :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
