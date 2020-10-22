ee28272fb3063b8c4fbafb60710e548f2e27416f
Tomoya Otake <tomoya.ton@gmail.com>
2020-10-22 12:04:18 +0900

    Update packages
    - company-emoji-20201020.554
    - forge-20201019.1615
    - helm-20201019.715
    - helm-core-20201019.715
    - lsp-mode-20201021.1658
    - magit-20201020.2130
    - request-20201018.1607
    - rg-20201018.1400
    - transient-20201020.1707

---
+++ elpa/company-emoji-20201020.554/company-emoji-pkg.el
@@ -0,0 +1,13 @@
+(define-package "company-emoji" "20201020.554" "company-mode backend for emoji"
+  '((cl-lib "0.5")
+    (company "0.8.0"))
+  :commit "514b33c29ff384188e90aceceabf305205837081" :keywords
+  '("emoji" "company")
+  :authors
+  '(("Alex Dunn" . "dunn.alex@gmail.com"))
+  :maintainer
+  '("Alex Dunn" . "dunn.alex@gmail.com")
+  :url "https://github.com/dunn/company-emoji.git")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20201021.1658/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20201021.1658" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "b76b85eca251e4573f03b4f69313e96aec628aa9" :keywords
+  '("languages")
+  :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
