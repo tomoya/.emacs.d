e8ee69800dffc7a14eaa9ecb3c7f3659531f1089
Tomoya Otake <tomoya.ton@gmail.com>
2020-12-25 10:18:17 +0900

    Remove obsolete wgrep (available wgrep is exist)

--- elpa/wgrep-20200203.830/wgrep-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; wgrep-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "wgrep" "../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep.el"
-;;;;;;  "bb3020a79eb03c8c87686b05fc58c061")
-;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep.el
-
-(autoload 'wgrep-setup "wgrep" "\
-Setup wgrep preparation." nil nil)
-
-(add-hook 'grep-setup-hook 'wgrep-setup)
-
-;;;### (autoloads "actual autoloads are elsewhere" "wgrep" "../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "wgrep" '("wgrep-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep-autoloads.el"
-;;;;;;  "../../../../../../../.config/emacs/elpa/wgrep-20200203.830/wgrep.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; wgrep-autoloads.el ends here
