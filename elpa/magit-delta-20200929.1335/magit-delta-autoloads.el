60739e63f369278cac29c95a6522603ab9223c50
Tomoya Otake <tomoya.ton@gmail.com>
2020-12-28 23:31:49 +0900

    Add magit-delta

---
+++ elpa/magit-delta-20200929.1335/magit-delta-autoloads.el
@@ -0,0 +1,48 @@
+;;; magit-delta-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "magit-delta" "../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta.el"
+;;;;;;  "7fe25738b8d573981c16cb9f3c2373d0")
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta.el
+
+(autoload 'magit-delta-mode "magit-delta" "\
+Use Delta when displaying diffs in Magit.
+
+If called interactively, enable Magit-Delta mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
+https://github.com/dandavison/delta
+
+\(fn &optional ARG)" t nil)
+
+;;;### (autoloads "actual autoloads are elsewhere" "magit-delta"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "magit-delta" '("magit-delta-")))
+
+;;;***
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/magit-delta-20200929.1335/magit-delta.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; magit-delta-autoloads.el ends here
