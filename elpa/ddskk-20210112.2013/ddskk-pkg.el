96150f259e422a1355c555891dfa20736ac9fc5c
Tomoya Otake <tomoya.ton@gmail.com>
2021-01-14 10:27:42 +0900

    Update packages
    - ddskk-20210112.2013
    - diff-hl-20210113.1930
    - forge-20210112.1148
    - helm-20210111.2036
    - ht-20210113.822
    - lsp-mode-20210113.2045
    - markdown-mode-20210112.1557
    - posframe-20210113.314
    - rg-20210113.1559

---
+++ elpa/ddskk-20210112.2013/ddskk-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ddskk" "20210112.2013" "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754"))
+  :commit "1cd0f65e4e116aaa1dddce98e95ce79911ff85ac")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-mode-20210113.2045/lsp-mode-pkg.el
@@ -0,0 +1,19 @@
+(define-package "lsp-mode" "20210113.2045" "LSP mode"
+  '((emacs "26.1")
+    (dash "2.14.1")
+    (dash-functional "2.14.1")
+    (f "0.20.0")
+    (ht "2.0")
+    (spinner "1.7.3")
+    (markdown-mode "2.3")
+    (lv "0"))
+  :commit "c8aa373a4d7ccc8f65d5cffa045de5883649a78a" :authors
+  '(("Vibhav Pant, Fangrui Song, Ivan Yonchovski"))
+  :maintainer
+  '("Vibhav Pant, Fangrui Song, Ivan Yonchovski")
+  :keywords
+  '("languages")
+  :url "https://github.com/emacs-lsp/lsp-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
