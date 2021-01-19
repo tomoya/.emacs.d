ff898b49dd6238f780b0d96746f4d26d8b2dee8e
Tomoya Otake <tomoya.ton@gmail.com>
2021-01-20 00:37:05 +0900

    Update packages
    - company-20210118.6
    - dash-20210118.1831
    - helm-20210119.746
    - ht-20210119.741
    - lsp-mode-20210119.1120
    - lsp-ui-20210118.436
    - magit-20210117.2011
    - posframe-20210118.42
    - transient-20210117.2008
    - with-editor-20210117.2008

---
+++ elpa/dash-20210118.1831/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20210118.1831" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "9ad0d2b972582d43ae19de4111d1f182cbbd2256" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/ht-20210119.741/ht-autoloads.el
@@ -0,0 +1,29 @@
+;;; ht-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "ht" "../../../../../../../../../.config/emacs/elpa/ht-20210119.741/ht.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/ht-20210119.741/ht.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ht" 'nil))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/ht-20210119.741/ht-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/ht-20210119.741/ht.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ht-autoloads.el ends here
