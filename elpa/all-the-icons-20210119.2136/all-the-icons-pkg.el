0cef6f63a8ccd5869ffb83a835f96c2339d9b9de
Tomoya Otake <tomoya.ton@gmail.com>
2021-01-24 23:23:27 +0900

    Update packages
    - all-the-icons-20210119.2136
    - company-20210122.2314
    - dash-20210123.1222
    - diff-hl-20210121.218
    - git-link-20210121.235
    - helm-20210121.2112
    - helm-core-20210120.757
    - lsp-mode-20210124.604
    - lsp-ui-20210121.1429
    - magit-20210123.446
    - markdown-mode-20210123.1547

---
+++ elpa/all-the-icons-20210119.2136/all-the-icons-pkg.el
@@ -0,0 +1,12 @@
+(define-package "all-the-icons" "20210119.2136" "A library for inserting Developer icons"
+  '((emacs "24.3"))
+  :commit "050d4c5a20f686c67ec1fc5eda30481d5a23383e" :authors
+  '(("Dominic Charlesworth" . "dgc336@gmail.com"))
+  :maintainer
+  '("Dominic Charlesworth" . "dgc336@gmail.com")
+  :keywords
+  '("convenient" "lisp")
+  :url "https://github.com/domtronn/all-the-icons.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/lsp-ui-20210121.1429/lsp-ui-pkg.el
@@ -0,0 +1,16 @@
+(define-package "lsp-ui" "20210121.1429" "UI modules for lsp-mode"
+  '((emacs "26.1")
+    (dash "2.14")
+    (dash-functional "1.2.0")
+    (lsp-mode "6.0")
+    (markdown-mode "2.3"))
+  :commit "56b0e04ccbe3d3ddb498728e6ae5431eb37e4e6a" :authors
+  '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
+  :maintainer
+  '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
+  :keywords
+  '("languages" "tools")
+  :url "https://github.com/emacs-lsp/lsp-ui")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
