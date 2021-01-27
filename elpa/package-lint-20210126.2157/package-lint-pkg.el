7b24cd249316be9e9bae70bfd95d8a1fc240d7b8
Tomoya Otake <tomoya.ton@gmail.com>
2021-01-27 10:42:58 +0900

    Update packages
    - dash-20210124.1524
    - git-commit-20210124.1829
    - lsp-mode-20210126.1836
    - magit-20210124.2150
    - package-lint-20210126.2157
    - projectile-20210125.726

---
+++ elpa/package-lint-20210126.2157/package-lint-pkg.el
@@ -0,0 +1,15 @@
+(define-package "package-lint" "20210126.2157" "A linting library for elisp package authors"
+  '((cl-lib "0.5")
+    (emacs "24.1")
+    (let-alist "1.0.6"))
+  :commit "f11ed5f3b73eacd40e9c7876e979fee66086c1e0" :authors
+  '(("Steve Purcell" . "steve@sanityinc.com")
+    ("Fanael Linithien" . "fanael4@gmail.com"))
+  :maintainer
+  '("Steve Purcell" . "steve@sanityinc.com")
+  :keywords
+  '("lisp")
+  :url "https://github.com/purcell/package-lint")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
