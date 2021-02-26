28d7f6faf760141a492dc4118e0eb06b53d81b82
Tomoya Otake <tomoya.ton@gmail.com>
2021-02-26 22:20:33 +0900

    Add tree-sitter and tree-sitter-langs packages

---
+++ elpa/tsc-20210116.621/tsc-autoloads.el
@@ -0,0 +1,41 @@
+;;; tsc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads "actual autoloads are elsewhere" "tsc" "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tsc" '("tsc-")))
+
+;;;***
+
+;;;### (autoloads "actual autoloads are elsewhere" "tsc-dyn-get"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-dyn-get.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from ../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-dyn-get.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "tsc-dyn-get" '("tsc-")))
+
+;;;***
+
+;;;### (autoloads nil nil ("../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-autoloads.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-dyn-get.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-obsolete.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc-pkg.el"
+;;;;;;  "../../../../../../../../../.config/emacs/elpa/tsc-20210116.621/tsc.el")
+;;;;;;  (0 0 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tsc-autoloads.el ends here
