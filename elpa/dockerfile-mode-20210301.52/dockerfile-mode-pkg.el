11998ddca3efb42b4bbcd4f72098cb7eaa3c07ee
Tomoya Otake <tomoya.ton@gmail.com>
2021-03-02 11:18:55 +0900

    Update packages
    - all-the-icons-20210228.1440
    - company-tabnine-20210228.435
    - dash-20210228.2221
    - dockerfile-mode-20210301.52
    - forge-20210228.1555
    - ghub-20210227.1925
    - lsp-mode-20210301.2001
    - magit-20210228.1841
    - package-lint-20210227.314
    - rg-20210227.1113
    - transient-20210228.1207
    - tree-sitter-langs-20210228.1450

---
+++ elpa/dockerfile-mode-20210301.52/dockerfile-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/tomoya/.config/emacs/elpa/dockerfile-mode-20210301.52/dockerfile-mode.el  -*- no-byte-compile: t -*-
+(define-package "dockerfile-mode" "20210301.52" "Major mode for editing Docker's Dockerfiles" '((emacs "24")) :commit "07dde72b0e356d772fb65b969bd6decfa166e4d7" :url "https://github.com/spotify/dockerfile-mode")
---
+++ elpa/rg-20210227.1113/rg-pkg.el
@@ -0,0 +1,15 @@
+(define-package "rg" "20210227.1113" "A search tool based on ripgrep"
+  '((emacs "25.1")
+    (transient "0.3.0")
+    (wgrep "2.1.10"))
+  :commit "8a537d9f033ba1b47f2ebe4d76f68b3dd74aefcb" :authors
+  '(("David Landell" . "david.landell@sunnyhill.email")
+    ("Roland McGrath" . "roland@gnu.org"))
+  :maintainer
+  '("David Landell" . "david.landell@sunnyhill.email")
+  :keywords
+  '("matching" "tools")
+  :url "https://github.com/dajva/rg.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
