f0203f1977da12a2360999bda18dfc07d607de8e
Tomoya Otake <tomoya.ton@gmail.com>
2021-04-21 15:54:46 +0900

    Update packages
    - all-the-icons-20210418.1920
    - direnv-20210419.1851
    - helm-20210419.1836
    - helm-core-20210419.1836
    - lsp-mode-20210419.1937
    - magit-20210421.155
    - php-mode-20210418.2325
    - posframe-20210421.412
    - transient-20210420.1635

---
+++ elpa/php-mode-20210418.2325/php-mode-pkg.el
@@ -0,0 +1,12 @@
+(define-package "php-mode" "20210418.2325" "Major mode for editing PHP code"
+  '((emacs "25.2"))
+  :commit "c86190eebf66a45791f30b9691da3d737cb8aae7" :authors
+  '(("Eric James Michael Ritz"))
+  :maintainer
+  '("USAMI Kenta" . "tadsan@zonu.me")
+  :keywords
+  '("languages" "php")
+  :url "https://github.com/emacs-php/php-mode")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
