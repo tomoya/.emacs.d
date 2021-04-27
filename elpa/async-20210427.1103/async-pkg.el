58a43c4650b38163084ce90c624a2884aa1a6777
Tomoya Otake <tomoya.ton@gmail.com>
2021-04-28 00:14:40 +0900

    Update packages
    - async-20210427.1103
    - forge-20210426.2126
    - ghub-20210427.1239
    - transient-20210427.833
    - with-editor-20210427.1244

---
+++ elpa/async-20210427.1103/async-pkg.el
@@ -0,0 +1,12 @@
+(define-package "async" "20210427.1103" "Asynchronous processing in Emacs"
+  '((emacs "24.3"))
+  :commit "943a75c6a5375de132c32914e0fd0a0d368931f2" :authors
+  '(("John Wiegley" . "jwiegley@gmail.com"))
+  :maintainer
+  '("John Wiegley" . "jwiegley@gmail.com")
+  :keywords
+  '("convenience" "async")
+  :url "https://github.com/jwiegley/emacs-async")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
