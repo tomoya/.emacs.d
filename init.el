d4ec7afb9869f4a8be18f8b3ce59c79114c2851c
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-18 16:06:28 +0900

    Change indentation naturally

--- init.el
+++ init.el
@@ -8,9 +8,9 @@
 
 ;; Set PATH env and exec-path from $PATH
 (setenv "PATH" (shell-command-to-string "echo $PATH"))
-(cl-loop for x in (reverse
-                (split-string (substring (shell-command-to-string "echo $PATH") 0 -1) ":"))
-      do (add-to-list 'exec-path x))
+(cl-loop for x
+         in (reverse (split-string (substring (shell-command-to-string "echo $PATH") 0 -1) ":"))
+         do (add-to-list 'exec-path x))
 
 ;; Used by custom-set-variables
 (require 'helm-projectile nil t)
