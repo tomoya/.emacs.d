344b7c0bbb6603ae0c6e948ca02ae8b95a4ea1c7
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-26 12:03:39 +0900

    Update packages
    - all-the-icons-20210522.1337
    - company-20210525.140
    - ddskk-20210522.348
    - diff-hl-20210523.11
    - forge-20210525.1345
    - git-commit-20210525.844
    - helm-20210525.659
    - helm-core-20210525.2002
    - lsp-mode-20210525.2051
    - lsp-ui-20210521.1912
    - magit-20210525.844
    - magit-section-20210525.844
    - markdown-mode-20210521.422
    - php-mode-20210519.1728
    - projectile-20210525.709
    - transient-20210525.1141
    - with-editor-20210524.1654

---
+++ elpa/ddskk-20210522.348/ddskk-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ddskk" "20210522.348" "Simple Kana to Kanji conversion program."
+  '((ccc "1.43")
+    (cdb "20141201.754"))
+  :commit "ce0517127586e26f95f94f45d22a832f40a28321")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
