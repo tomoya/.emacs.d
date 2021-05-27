cfe9a1cefdd98d3bde42a5b5c39e2429cb7e55b5
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-27 10:48:25 +0900

    Update packages
    - all-the-icons-20210522.1337
    - ghub-20210427.1239

--- elpa/all-the-icons-20210526.2356/all-the-icons-pkg.el
+++ elpa/all-the-icons-20210526.2356/all-the-icons-pkg.el
@@ -1,6 +1,6 @@
-(define-package "all-the-icons" "20210522.1337" "A library for inserting Developer icons"
+(define-package "all-the-icons" "20210526.2356" "A library for inserting Developer icons"
   '((emacs "24.3"))
-  :commit "d711dc10b2504c06500c171ea5860f15827fdb72" :authors
+  :commit "d3f994916cd0bc7ff1ace52fe275cca1c6f75d18" :authors
   '(("Dominic Charlesworth" . "dgc336@gmail.com"))
   :maintainer
   '("Dominic Charlesworth" . "dgc336@gmail.com")
--- elpa/ghub-20210526.1055/ghub-pkg.el
+++ elpa/ghub-20210526.1055/ghub-pkg.el
@@ -1,8 +1,8 @@
-(define-package "ghub" "20210427.1239" "Minuscule client libraries for Git forge APIs."
+(define-package "ghub" "20210526.1055" "Minuscule client libraries for Git forge APIs."
   '((emacs "25.1")
     (let-alist "1.0.5")
     (treepy "0.1.1"))
-  :commit "d6e6b0666104f3896d05d2b03d08d84d9dca096f" :authors
+  :commit "ae59388adbba32fa00e39f3323fe69367739ee6f" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
