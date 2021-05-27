46d145e31be6ae3a6c46f40d0c71bce6d1d40581
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-28 00:32:41 +0900

    Set oderless functions to affe-*-function

--- init.el
+++ init.el
@@ -91,6 +91,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(affe-highlight-function 'orderless-highlight-matches)
+ '(affe-regexp-function 'orderless-pattern-compiler)
  '(ansi-color-names-vector
    ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(auto-revert-interval 1)
