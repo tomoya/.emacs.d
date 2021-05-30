bc9fbc33b50b59025bfe23d94c82b9546bde80e7
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-30 21:43:06 +0900

    Add consult-lsp package

---
+++ elpa/consult-lsp-20210428.1515/consult-lsp-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from consult-lsp.el  -*- no-byte-compile: t -*-
+(define-package "consult-lsp" "20210428.1515" "LSP-mode Consult integration" '((emacs "27.1") (lsp-mode "5.0") (consult "0.6") (f "0.20.0")) :commit "12989949cc21a1173206f688d56a1e798073a4c3" :authors '(("Gerry Agbobada")) :maintainer '("Gerry Agbobada") :keywords '("tools" "completion" "lsp") :url "https://github.com/gagbo/consult-lsp")
