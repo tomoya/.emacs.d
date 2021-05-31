fa1446e914c1e83ed7cc41b683ef0727997a1a8e
Tomoya Otake <tomoya.ton@gmail.com>
2021-05-31 23:30:49 +0900

    Use fd instead of find

--- init.el
+++ init.el
@@ -91,6 +91,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(affe-find-command "fd --full-path --follow")
  '(affe-highlight-function 'orderless-highlight-matches)
  '(affe-regexp-function 'orderless-pattern-compiler)
  '(ansi-color-names-vector
@@ -112,6 +113,7 @@
  '(company-tooltip-idle-delay 0.1)
  '(company-tooltip-limit 20)
  '(completion-styles '(orderless flex partial-completion))
+ '(consult-find-command "fd --full-path --follow")
  '(create-lockfiles nil)
  '(css-indent-offset 2)
  '(cursor-in-non-selected-windows nil)
