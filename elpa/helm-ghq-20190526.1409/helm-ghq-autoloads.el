827a25e793643cbec708ce008eb0903b587d3395
Tomoya Otake <tomoya.ton@gmail.com>
2021-06-01 16:03:57 +0900

    Delete unused packages
    - helm-descbinds
    - helm-ghq

--- elpa/helm-ghq-20190526.1409/helm-ghq-autoloads.el
+++
@@ -1,37 +0,0 @@
-;;; helm-ghq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "helm-ghq" "../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el"
-;;;;;;  "648a483232f303691d75c99b76600b67")
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el
-
-(autoload 'helm-ghq "helm-ghq" nil t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "helm-ghq" "../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "helm-ghq" '("helm-ghq-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq-autoloads.el"
-;;;;;;  "../../../../../../../.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; helm-ghq-autoloads.el ends here
--- elpa/helm-ghq-20190526.1409/helm-ghq-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from /Users/tomoya/.emacs.d/elpa/helm-ghq-20190526.1409/helm-ghq.el  -*- no-byte-compile: t -*-
-(define-package "helm-ghq" "20190526.1409" "Ghq with helm interface" '((emacs "24") (helm "2.2.0")) :commit "d0d6aa0f407388e7012f0443df8ae657ece01779" :authors '(("Takashi Masuda" . "masutaka.net@gmail.com")) :maintainer '("Takashi Masuda" . "masutaka.net@gmail.com") :url "https://github.com/masutaka/emacs-helm-ghq")
