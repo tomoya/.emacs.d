509b9aa6df5ca107664936ab946795755e6c769b
Tomoya Otake <tomoya.ton@gmail.com>
2021-06-10 15:59:58 +0900

    Update fd command options

--- init.el
+++ init.el
@@ -91,7 +91,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(affe-find-command "fd --full-path --follow")
+ '(affe-find-command "fd --color=never --full-path")
  '(affe-highlight-function 'orderless-highlight-matches)
  '(affe-regexp-function 'orderless-pattern-compiler)
  '(ansi-color-names-vector
@@ -113,7 +113,7 @@
  '(company-tooltip-idle-delay 0.1)
  '(company-tooltip-limit 20)
  '(completion-styles '(orderless flex partial-completion))
- '(consult-find-command "fd --full-path --follow ARG OPTS")
+ '(consult-find-command "fd --color=never --full-path ARG OPTS")
  '(create-lockfiles nil)
  '(css-indent-offset 2)
  '(cursor-in-non-selected-windows nil)
