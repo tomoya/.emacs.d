ef1eefd26c51364851b86134e31513d4a16414e3
Tomoya Otake <tomoya.ton@gmail.com>
2021-06-25 09:51:19 +0900

    Add consult-flycheck package

---
+++ elpa/consult-flycheck-20210530.202/consult-flycheck-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from consult-flycheck.el  -*- no-byte-compile: t -*-
+(define-package "consult-flycheck" "20210530.202" "Provides the command `consult-flycheck'" '((consult "0.8") (flycheck "31") (emacs "26.1")) :commit "92b259e6a8ebe6439f67d3d7ffa44b7e64b76478" :authors '(("Daniel Mendler and Consult contributors")) :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/consult")
