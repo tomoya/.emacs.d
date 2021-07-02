a66de0c7747c0e050bd0e07f080649156cb7c27b
Tomoya Otake <tomoya.ton@gmail.com>
2021-07-03 00:06:35 +0900

    Update packages
    - git-commit-20210702.822
    - lsp-mode-20210702.831
    - magit-20210702.822
    - magit-section-20210702.822
    - marginalia-20210701.2054
    - projectile-20210701.441
    - spinner-1.7.4
    - transient-20210701.1116

---
+++ elpa/marginalia-20210701.2054/marginalia-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from marginalia.el  -*- no-byte-compile: t -*-
+(define-package "marginalia" "20210701.2054" "Enrich existing commands with completion annotations" '((emacs "26.1")) :commit "3bf0a4db55f6267467f0a08715f4776509a3b503" :authors '(("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de")) :maintainer '("Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler" . "mail@daniel-mendler.de") :url "https://github.com/minad/marginalia")
