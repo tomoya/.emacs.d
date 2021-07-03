c006d4fd7d44df1ea68861eb6086eed67e48ece7
Tomoya Otake <tomoya.ton@gmail.com>
2021-07-04 00:11:28 +0900

    Add ssh-config-mode package

---
+++ elpa/ssh-config-mode-20210703.951/ssh-config-mode-pkg.el
@@ -0,0 +1,10 @@
+(define-package "ssh-config-mode" "20210703.951" "Mode for fontification of ~/.ssh/config" 'nil :commit "41deddfd39e6461526037570e800ada9a0ae7fa7" :authors
+  '(("Harley Gorrell" . "harley@panix.com"))
+  :maintainer
+  '("Harley Gorrell" . "harley@panix.com")
+  :keywords
+  '("ssh" "config" "emacs")
+  :url "https://github.com/jhgorrell/ssh-config-mode-el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
