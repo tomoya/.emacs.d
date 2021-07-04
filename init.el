28559b573af48f6d9a2d7eb8d7293ef8407fafec
Tomoya Otake <tomoya.ton@gmail.com>
2021-07-05 01:28:59 +0900

    Disable company-tabnime

--- init.el
+++ init.el
@@ -339,7 +339,7 @@ point reaches the beginning or end of the buffer, stop there."
                           company-preview-frontend))
 (push 'company-emoji company-backends)
 (push 'company-lsp company-backends)
-(push 'company-tabnine company-backends)
+;; (push 'company-tabnine company-backends)
 (add-hook 'company-mode-hook 'company-box-mode)
 
 ;; undo-tree
