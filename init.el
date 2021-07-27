2f301aaca7bbc66a9eee646953a4d336f83d856e
Tomoya Otake <tomoya.ton@gmail.com>
2021-07-28 01:04:29 +0900

    Change straight-check-for-modifications for performance improvement

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 
 ;; straight
 (defvar bootstrap-version)
+(defvar straight-check-for-modifications '(check-on-save find-when-checking))
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
       (bootstrap-version 5))
