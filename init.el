72ccf72f3f169cd64e057c5f0f9d5510f2eb2d1e
Tomoya Otake <tomoya.ton@gmail.com>
2021-07-27 17:04:44 +0900

    Add straight.el package

--- init.el
+++ init.el
@@ -4,6 +4,20 @@
 (require 'cl-lib)
 (setq default-directory "~/")
 
+;; straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 (add-to-list 'load-path (locate-user-emacs-file "nano"))
 
 ;; Set PATH env and exec-path from $PATH
---
+++ straight/build/straight/straight-x.el
@@ -0,0 +1,1 @@
+/Users/tomoya/.config/emacs/straight/repos/straight.el/straight-x.el
