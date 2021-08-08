dc47e42263e1c8ecf3040d808bcb40dfd34bf333
Tomoya Otake <tomoya.ton@gmail.com>
2021-08-09 01:10:30 +0900

    Use consult-find-config instead of consult-find-command

--- config/custom.el
+++ config/custom.el
@@ -28,7 +28,8 @@
  '(company-tooltip-idle-delay 0.1)
  '(company-tooltip-limit 20)
  '(completion-styles '(orderless flex partial-completion))
- '(consult-find-command "fd --color=never --full-path ARG OPTS")
+ '(consult-find-config
+   '(:args "fd --color=never --full-path" :command consult--find-command-builder :highlight consult--command-highlight))
  '(create-lockfiles nil)
  '(css-indent-offset 2)
  '(cursor-in-non-selected-windows nil)
