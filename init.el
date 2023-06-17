9162ad8b987563d31aefabf35dc722a15a322069
Tomoya Otake <tomoya.ton@gmail.com>
2023-06-18 02:00:38 +0900

    Add after-init-hook to my/emacs-init-time

--- init.el
+++ init.el
@@ -584,4 +584,43 @@ If buffer is associated with a file name, add that file to the
 (put 'list-timers 'disabled nil)
 (put 'list-threads 'disabled nil)
 
+
+(defconst my/before-load-init-time (current-time))
+
+;;;###autoload
+(defun my/load-init-time ()
+  "Loading time of user init files including time for `after-init-hook'."
+  (let ((time1 (float-time
+                (time-subtract after-init-time my/before-load-init-time)))
+        (time2 (float-time
+                (time-subtract (current-time) my/before-load-init-time))))
+    (message (concat "Loading init files: %.0f [msec], "
+                     "of which %.f [msec] for `after-init-hook'.")
+             (* 1000 time1) (* 1000 (- time2 time1)))))
+(add-hook 'after-init-hook #'my/load-init-time t)
+
+(defvar my/tick-previous-time my/before-load-init-time)
+
+;;;###autoload
+(defun my/tick-init-time (msg)
+  "Tick boot sequence at loading MSG."
+  (when my/loading-profile-p
+    (let ((ctime (current-time)))
+      (message "---- %5.2f[ms] %s"
+               (* 1000 (float-time
+                        (time-subtract ctime my/tick-previous-time)))
+               msg)
+      (setq my/tick-previous-time ctime))))
+
+(defun my/emacs-init-time ()
+  "Emacs booting time in msec."
+  (interactive)
+  (message "Emacs booting time: %.0f [msec] = `emacs-init-time'."
+           (* 1000
+              (float-time (time-subtract
+                           after-init-time
+                           before-init-time)))))
+
+(add-hook 'after-init-hook #'my/emacs-init-time)
+
 ;;; init.el ends here
